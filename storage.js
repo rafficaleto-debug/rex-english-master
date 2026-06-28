4be212e652f013cec5ea6439a800ff68e912de2f
rafficaleto-debug <raffica.leto@icloud.com>
2026-06-29 00:03:33 +0900

    Add files via upload

--- storage.js
+++ storage.js
@@ -1,7 +1,7 @@
 
 (function(){
-  var APP_VERSION='42.0.0';
-  var STORE_KEY='rexEnglishMaster.profile.v42';
+  var APP_VERSION='43.0.0';
+  var STORE_KEY='rexEnglishMaster.profile.v43';
   var defaultProfile={
     schemaVersion:1, appVersion:APP_VERSION, contentVersion:window.REX_CONTENT_VERSION || 'unknown',
     added:[], weak:[], fav:[], mistakes:[],
@@ -15,12 +15,12 @@
     var p=readJson(STORE_KEY,null);
     if(p && p.schemaVersion){ return p; }
     var migrated=clone(defaultProfile);
-    migrated.added=readJson('added_v42', readJson('added_v42', []));
-    migrated.weak=readJson('weak_v42', readJson('weak_v42', []));
-    migrated.fav=readJson('fav_v42', readJson('fav_v42', []));
-    migrated.mistakes=readJson('mistakes_v42', readJson('mistakes_v42', []));
-    migrated.score=readJson('score_v42', readJson('score_v42', defaultProfile.score));
-    migrated.chats=readJson('chats_v42', []);
+    migrated.added=readJson('added_v43', readJson('added_v43', []));
+    migrated.weak=readJson('weak_v43', readJson('weak_v43', []));
+    migrated.fav=readJson('fav_v43', readJson('fav_v43', []));
+    migrated.mistakes=readJson('mistakes_v43', readJson('mistakes_v43', []));
+    migrated.score=readJson('score_v43', readJson('score_v43', defaultProfile.score));
+    migrated.chats=readJson('chats_v43', []);
     saveProfile(migrated);
     return migrated;
   }
