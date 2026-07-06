d10faeeaf822667d048ebae5d636bbea317f7d46
rafficaleto-debug <raffica.leto@icloud.com>
2026-07-06 21:38:04 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', event => { self.skipWaiting(); });
+self.addEventListener('activate', event => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map(k => caches.delete(k)));
+    await self.clients.claim();
+  })());
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request, { cache: 'no-store' }).catch(() => caches.match(event.request)));
+});
